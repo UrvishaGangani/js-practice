0d6d69add3a495cf37d5614ca3a4105b70921a9d
UrvishaGangani <96762066+UrvishaGangani@users.noreply.github.com>
2025-10-30 14:34:01 +0000

    start colorchanger project

--- 01_basics/test.js
+++ 01_basics/test.js
@@ -1,1 +1,49 @@
-console.log("Hello World..");
+// const min = 0 
+// const max = 6
+
+// const randomNumber = Math.floor(Math.random() * (max-min +1));
+
+// console.log(randomNumber)
+
+// let myDate = new Date()
+// console.log(myDate)
+// console.log(myDate.toString());
+// console.log(myDate.toLocaleString() );
+// console.log(typeof myDate);
+
+// const userDetail = new Map();
+// userDetail.set('ID', '101')
+// userDetail.set('Email', 'abc@gmail.com')
+// userDetail.set('ActiveStatus', '0')
+
+// for (const [key, value] of userDetail) {
+//   console.log(key, ":-", value);
+// }
+
+// const myObject = {
+//     Id: '1001',
+//     Email: 'abcccc@gmail.com',
+//     fname: 'abc',
+//     lname:'xyz'
+// }
+
+// for (const key in myObject) {
+//     console.log(key , ' : ',myObject[key])
+// }
+
+
+// const coding = ["JS", "Python", "Html", "CSS","Ruby","React","React.js","React Native"]
+// coding.forEach(function (item) {
+//     console.log(item)
+// })
+
+// let result = coding.filter((item) => item.startsWith("R"))
+// console.log(result)
+// console.log(`gameName Object output -- ${gameName} \n gName string output -- ${gName}`)
+
+const mynums = [1, 2, 3, 4, 5, 6, 7]
+const result = mynums.reduce(function (acc, currentvalue) {
+    console.log(`acc: ${acc} and Current value : ${currentvalue}`)
+    return acc + currentvalue
+}, 0)
+console.log(result)
