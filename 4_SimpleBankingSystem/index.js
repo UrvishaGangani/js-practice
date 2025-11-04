f329bbfabf7a41501214238af0c84d80d8e88fe8
UrviGangani <202152923+UrviGangani@users.noreply.github.com>
2025-11-04 14:30:45 +0000

    Get GitHub Userdata From Api and Display then into card Complete

--- 4_SimpleBankingSystem/index.js
+++ 4_SimpleBankingSystem/index.js
@@ -1,7 +1,54 @@
-import BankAccount from "./bankAccount";
+// import BankAccount from "./bankAccount.js";
 
-const myAccount = new BankAccount('001', 'John Doe', 1000);
+let currentAccount = null;
 
-myAccount.deposit(500);
-myAccount.withraw(200);
-myAccount.checkBalance();
+//select DOM elements
+
+const createForm = document.getElementById("creatAccountForm");
+const actionsSection = document.getElementById("actions");
+const info = document.getElementById("info");
+const accountDisplay = document.getElementById("accountDisplay");
+
+const depositeBtn = document.getElementById('depositeBtn');
+const withdrawBtn = document.getElementById('withdrawBtn');
+const balanceBtn = document.getElementById('balanceBtn')
+
+createForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+
+    const holder = document.getElementById('accountHolder').value;
+    const number = document.getElementById('accountNumber').value;
+
+    currentAccount = new BankAccount(number, holder, 0);
+    info.textContent = `🎉 Account created for ${holder} (Account #${number})`;
+    actionsSection.style.display = "block";
+    accountDisplay.textContent = currentAccount.checkBalance();
+
+    createForm.reset();
+
+});
+
+
+// Deposit money
+depositBtn.addEventListener('click', () => {
+  const amount = parseFloat(prompt("Enter deposit amount:"));
+  if (isNaN(amount)) return alert("Invalid amount!");
+  const result = currentAccount.deposit(amount);
+  accountDisplay.textContent = result;
+});
+
+// Withdraw money
+withdrawBtn.addEventListener('click', () => {
+  const amount = parseFloat(prompt("Enter withdrawal amount:"));
+  if (isNaN(amount)) return alert("Invalid amount!");
+  const result = currentAccount.withdraw(amount);
+  accountDisplay.textContent = result;
+});
+
+// Check balance
+balanceBtn.addEventListener('click', () => {
+  const result = currentAccount.checkBalance();
+  accountDisplay.textContent = result;
+});
+
+
---
+++ 5_API_Practice/index.js
@@ -0,0 +1,53 @@
+const reqUrl = "https://api.github.com/users";
+// const req = new XMLHttpRequest();
+// req.open('GET', reqUrl)
+// req.onreadystatechange = function () {
+//     if (req.readyState === 4) {
+//         const data = JSON.parse(this.responseText);
+//         console.log("Response :: " + data);
+//         if (Array.isArray(data)) {
+//             data.forEach(user => {
+//                 console.log(user);
+//             })
+//         }
+//         else {
+//             console.error('expected an array but got object');
+//         }
+        
+//     }
+// }
+// req.send();
+
+async function createUserCards() {
+  try {
+    const res = await fetch(reqUrl);
+    const users = await res.json();
+
+      const container = document.getElementById("usersCard");
+      container.innerHTML = "";
+
+      users.forEach(user => {
+          // Create card element
+          const card = document.createElement("div");
+          card.className = "card";
+          
+          // Add HTML structure
+          card.innerHTML = `
+          <img src="${user.avatar_url}" alt="${user.login}" class="avatar"/>
+          <div class="name">${user.login}</div>
+          <div class="type">${user.type}</div>
+          <div class="links">
+            <a href="${user.html_url}" target="_blank">View Profile</a>
+            <a href="${user.repos_url}" target="_blank">Repos</a>
+          </div>
+          `;
+
+          //Add card to the page
+          container.appendChild(card);
+      });      
+  } catch (error) {
+    console.error("Error fetching user:", error);
+  }
+}
+
+createUserCards();
